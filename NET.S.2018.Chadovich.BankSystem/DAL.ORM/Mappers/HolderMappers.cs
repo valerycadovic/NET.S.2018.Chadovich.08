5878b6bdb80a254d8d6c9d2dac5086259fa5f6e2
Valery Čadovič <valerachad03@gmail.com>
2018-09-03 23:44:22 +0300

    bll refactoring

--- NET.S.2018.Chadovich.BankSystem/DAL.ORM/Mappers/HolderMappers.cs
+++ NET.S.2018.Chadovich.BankSystem/DAL.ORM/Mappers/HolderMappers.cs
@@ -8,6 +8,46 @@ namespace DAL.ORM.Mappers
 {
     public static class HolderMappers
     {
-        public static Holder ToOr
+        public static Holder ToOrmHolder(this HolderDTO holderDto)
+        {
+            if (holderDto is null)
+            {
+                throw new ArgumentNullException($"{nameof(holderDto)} is null");
+            }
+
+            return new Holder
+            {
+                Email = holderDto.Email,
+                FullName = holderDto.FullName,
+                IsActive = holderDto.IsActive,
+                PassportId = holderDto.PassportId,
+                Accounts = new HashSet<Account>
+                (
+                    from item in holderDto.Accounts
+                    select item.ToOrmAccount()
+                )
+            };
+        }
+
+        public static HolderDTO ToHolderDto(this Holder holder)
+        {
+            if (holder is null)
+            {
+                throw new ArgumentNullException($"{nameof(holder)} is null");
+            }
+
+            return new HolderDTO
+            {
+                Email = holder.Email,
+                FullName = holder.FullName,
+                IsActive = holder.IsActive,
+                PassportId = holder.PassportId,
+                Accounts = new HashSet<AccountDTO>
+                (
+                    from item in holder.Accounts
+                    select item.ToAccountDto()
+                )
+            };
+        }
     }
 }
